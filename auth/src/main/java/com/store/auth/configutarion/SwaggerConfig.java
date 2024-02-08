66497d51e73ac29d373cba284cb781599f9df88c
Luiz Gustavo do Amaral Correa <gustavo.lgac@gmail.com>
2024-02-08 14:56:11 -0300

    Swagger configuration

---
+++ auth/src/main/java/com/store/auth/configutarion/SwaggerConfig.java
@@ -0,0 +1,13 @@
+package com.store.auth.configutarion;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(info=@Info(title = "Titulo da minha doc", description = "Microsserviço de autenticação", version = "v1", license = @License(name = "MIT", url = "http://localhost")))
+public class SwaggerConfig {
+
+
+}
