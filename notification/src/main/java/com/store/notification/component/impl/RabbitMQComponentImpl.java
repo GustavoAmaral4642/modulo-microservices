f12f779334237dab91f1444554cabf801e7ed864
Luiz Gustavo do Amaral Correa <gustavo.lgac@gmail.com>
2024-02-28 15:05:32 -0300

    service order

--- notification/src/main/java/com/store/notification/component/impl/RabbitMQComponentImpl.java
+++ notification/src/main/java/com/store/notification/component/impl/RabbitMQComponentImpl.java
@@ -1,10 +1,15 @@
 package com.store.notification.component.impl;
 
+import com.netflix.discovery.converters.Auto;
 import com.store.notification.component.RabbitMQComponent;
 import com.store.notification.service.impl.EmailServiceImpl;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Map;
 
 @Component
 public class RabbitMQComponentImpl implements RabbitMQComponent {
@@ -13,6 +18,15 @@ public class RabbitMQComponentImpl implements RabbitMQComponent {
     @Value("${rabbitmq.queue.name}")
     private String queue;
 
+    @Autowired
+    private EmailServiceImpl emailService;
+
+    private final WebClient webClient;
+
+    public RabbitMQComponentImpl(WebClient webClient) {
+        this.webClient = webClient;
+    }
+
     // eu quero enxergar a order_notification (var ambiente)
     // esse RabbitListener vai pegar o item que vem da fila do rabbitMQ
     // e vai fazer o que quiser quando pegar
@@ -20,16 +34,25 @@ public class RabbitMQComponentImpl implements RabbitMQComponent {
     public void handleMessage(String message) {
 
         // anotação *1
-            // posso usar dessa forma
+        // posso usar dessa forma
         /*
         System.out.print("Recebi algo da fila" + queue);
         */
+        Map<String, Object> obj = emailService.convertToObject(message);
+        int user_id = (int) obj.get("user_id");
+        String product_name = (String) obj.get("product_name");
 
-        String obj = EmailServiceImpl.convertToObject(message);
-        String content = EmailServiceImpl.constructContent();
-        EmailServiceImpl.sendEmail("Conteúdo", obj);
-        // TODO receber o dado
-        // TODO identificar produto e usuário
-        // TOTO enviar um e-mail para um usuário
+        String response = this.webClient.get()
+                .uri("/user/"+ String.valueOf(user_id))
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+
+        Map<String, Object> user = emailService.convertToObject(response);
+
+        String content = emailService.constructContent(product_name, (String) user.get("username"));
+        emailService.sendEmail(content, (String) user.get("email"), "Notificação XPTO");
+
+
     }
 }
---
+++ order/src/main/java/com/store/order/domain/OrderItem.java
@@ -0,0 +1,32 @@
+package com.store.order.domain;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "`order_item`")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long product_id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="order_id")
+    @JsonBackReference
+    private Order order;
+}
